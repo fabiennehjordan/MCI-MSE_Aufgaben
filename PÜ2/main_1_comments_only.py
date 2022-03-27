b4a1e6b00ef5895e86e76911c4addcba7370f46a
Jennifer Ladig <jenny@ladig.de>
2022-03-27 18:26:29 +0200

    commit

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,21 +1,29 @@
 # UC 2.0 - Auswertung der Leistungsdaten
 
 #%% UC 2.1 Einlesen der Daten
- 
-
+ #Code sucht in vorgegebenen Ordner nach den Daten 
+ #Software importiert den Datensatz
 
 #%% UC 2.2 - Vorverarbeitung der Daten
-
+#Überprüfung auf valide Werte
+# 
 #%% UC 2.3 - Analysieren der Daten auf Abbruchkriterium
-
+#definiert eine Funktion valid()
+#Funktion wird als false angenommen, wenn der Puls 90% der maximalen Herzfrequenz entspricht
+#
+#
 
 #%% 2.4 - Erstellen einer Zusammenfassung
+# Erstellung eines txt.-files, welches die relevantesten Kennzahlen behinhaltet (z.B maximale Herzfrequenz, durchschnittliche Herzfrequenz,..)
 
 #%% 2.5 - Visualisierung der Daten
+#Graphen plotten mit matplotlib
+
 
 #%% 2.6 - Manuelle Eingabe eines Abbruch-Kriteriums
-
+# Pop-up Fenster, welches die Diagnostikerin nach der Gültigkeit des Testes abfrägt
 #%% 2.7 - Speichern der Daten
+#Speichern der vorverarbeiteten Daten und Plots unter einem seperaten Patientenpfad
 
 
 
