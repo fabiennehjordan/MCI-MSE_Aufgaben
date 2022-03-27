9b37342a102cbdf1b1de81068e671734a25b11e7
Jennifer Ladig <jenny@ladig.de>
2022-03-27 17:53:06 +0200

    comments Überschriften

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,4 +1,21 @@
 # UC 2.0
 
 #%% UC 2.1 Einlesen der Daten
+# 
+
+
+#%% UC 2.2 - Vorverarbeitung der Daten
+
+#%% UC 2.3 - Analysieren der Daten auf Abbruchkriterium
+
+
+#%% 2.4 - Erstellen einer Zusammenfassung
+
+#%% 2.5 - Visualisierung der Daten
+
+#%% 2.6 - Manuelle Eingabe eines Abbruch-Kriteriums
+
+#%% 2.7 - Speichern der Daten
+
+
 
