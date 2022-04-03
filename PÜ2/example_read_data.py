6dd42e9d64fc067ce779ac9177d57976bae213f0
David.Mikulic <davidmikulic.2000@gmail.com>
2022-04-03 19:17:33 +0200

    main_3_functional updated

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -6,14 +6,13 @@ import matplotlib.pyplot as plt
 for i in range(1,4):
   file_name =  'input_data/power_data_' + str(i) + '.txt'
   power_data_watts = open(file_name).read().split("\n")
+  
   data = np.array(power_data_watts, dtype=np.int16)
   plt.title("Line graph Patient " + str(i))
   plt.ylabel("Power in W")
   plt.xlabel("Time in s")
   plt.plot(data, color="red")
   plt.show()
-
-
 # %%
 
 # %%
